5c92fcf77e6bc47ff45682042e8157492e2e9b9c
Shohe Ohtani <>
2019-12-25 09:06:22 +0900

    +

--- functions/index.js
+++ functions/index.js
@@ -1,89 +1,31 @@
 const functions = require('firebase-functions');
-const url = require('url');
 const express = require('express');
-const path = require('path');
-const fs = require('fs');
-
+// const path = require('path');
 const app = express();
 
-// Create and Deploy Your First Cloud Functions
-// https://firebase.google.com/docs/functions/write-firebase-functions
+app.get('/users', (req, res) => {
+  // 本来はここでFireStoreとかからデータ取ってくるよ
+  const users = [
+    { "id": 1, "name": "イリヤ" },
+    { "id": 2, "name": "美遊" },
+    { "id": 3, "name": "クロエ" },
+    { "id": 4, "name": "オルタ" },
+    { "id": 5, "name": "マシュ" }
+  ];
 
-exports.list = functions.https.onRequest((request, response) => {
-    response.sendFile(path.join(__dirname + '/views/list.html'));
+  // データを返却
+  res.send(JSON.stringify(users));
 });
 
-exports.getRoomList = functions.https.onRequest((request, response) => {
-    let rooms = getRoomListFromJson(__dirname + '/json/room.json');
-    response.send(rooms);
-});
-
-exports.test = functions.https.onRequest((request, response) => {
-    response.sendFile(path.join(__dirname + '/views/index.html'));
-});
-
-exports.createRoom = functions.https.onRequest((request, response) => {
-    // get data
-    var url_parse = url.parse(request.url,true);
-    var roomName = url_parse.query["room-name"];
-    var word01 = url_parse.query["keyword01"];
-    var word02 = url_parse.query["keyword02"];
-    var keyWords = new Array(word01, word02);
-
-    var room = new Room(roomName, keyWords);
-    room.create(__dirname + '/json/room.json');
-    response.redirect('/wordwolf/us-central1/list');
-});
-
-
-class Room {
-
-    constructor(name, keyWords) {
-        this.name = name;
-        this.keyWords = keyWords;
-    }
-
-    create(path) {
-        if (this.isExistFile(path)) {
-            this.updateJsonFile(path);
-        } else {
-            writeJsonFile(path, new Array(this));
-        }
-    }
-
-    isExistFile(path) {
-        try {
-            fs.statSync(path);
-            return true;
-        } catch(err) {
-            if (err.code === 'ENOENT') return false;
-        }
-    }
-
-    updateJsonFile(path) {
-        let list = getRoomListFromJson(path);
-        list.push(this);
-        writeJsonFile(path, list);
-    }
-}
-
-// json file
-function getRoomListFromJson(path) {
-    let json = fs.readFileSync(path, 'utf-8');
-    let data = JSON.parse(json);
-    let roomList = new Array();
-    data.forEach(function(element) {
-        roomList.push(new Room(element['name'], element['keyWords']));
-    });
-    return roomList;
-}
-
-function writeJsonFile(path, rooms) {
-    fs.writeFileSync(path, JSON.stringify(rooms), (err) => {
-        if (err) rej(err);
-        if (!err) {
-            console.log('Success create Json');
-            console.log(this);
-        }
-    });
-}
+/*
+.
+├── firebase.json
+├── functions
+│   ├── index.js
+│   ├── node_modules
+│   ├── package-lock.json
+│   └── package.json
+└── public
+    ├── 404.html
+    └── index.html
+*/
